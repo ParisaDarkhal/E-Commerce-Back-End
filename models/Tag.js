06c929eb74269eccbb718851b6a049f6d8fc104d
ParisaDarkhal <111551848+ParisaDarkhal@users.noreply.github.com>
2023-05-14 13:37:05 -0400

    model tag set

--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: true, //i am not sure about this one, but I guess there might be products that don't have tag
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
