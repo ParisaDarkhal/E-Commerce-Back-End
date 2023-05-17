410369f85343bb17eacbb5a4b5a2b48e19b465a9
ParisaDarkhal <111551848+ParisaDarkhal@users.noreply.github.com>
2023-05-16 20:19:30 -0400

    get tag by id added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,9 +30,34 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// find a single tag by its `id` and be sure to include its associated Product data
+// localhot:3001/tags/:id
+router.get("/:id", async (req, res) => {
+  try {
+    const dbTagData = await Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Product,
+          attributes: ["id", "product_name", "price", "inStock", "category_id"],
+          through: ProductTag,
+          as: "product_tags",
+        },
+      ],
+    });
+
+    if (!dbTagData) {
+      res.status(404).json({ message: "Tag not found!" });
+      return;
+    }
+
+    res.json(dbTagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", (req, res) => {
