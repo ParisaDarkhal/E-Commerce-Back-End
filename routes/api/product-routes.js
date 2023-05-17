32a30a8d5a7ad987b91bc074bf5a1d5ac6161eb1
ParisaDarkhal <111551848+ParisaDarkhal@users.noreply.github.com>
2023-05-16 22:06:31 -0400

    req.body for post fixed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -77,8 +77,8 @@ router.post("/", (req, res) => {
     {
       product_name: "Basketball",
       price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      inStock: 3,
+      tag_id: [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
